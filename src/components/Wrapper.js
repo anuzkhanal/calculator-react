537de4071616456c0b8cc8fd3249d69780da527d
Anuj Khanal <anuj@Anujs-MacBook-Pro.local>
2022-01-24 19:18:39 +0200

    init commit

---
+++ src/components/Wrapper.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../css/wrapper.css'
+
+
+
+function Wrapper( {children}) {
+  return (
+  <div className='wrapper'> 
+      { children }
+  </div>
+  )
+
+
+}
+
+export default Wrapper;
